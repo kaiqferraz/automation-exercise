405c884e7f82782b8a7e3c22a3ce8a139cee5f5e
kaiqferraz <kaiqueferraz97@gmail.com>
2025-04-03 19:25:15 -0300

    Ajustado spec para adicionar produto ao carrinho

--- cypress/e2e/step_definitions/carrinhoIncluirProduto.feature
+++ cypress/e2e/step_definitions/carrinhoIncluirProduto.feature
@@ -2,11 +2,9 @@ Feature: Incluir produto no carrinho de compras
 
     Scenario: Usuário insere produto no carrinho de compras
 
-        Given que o usuário visita a página de produtos
-        When o usuário seleciona um produto
-        And insere a quantidade desejada
+        Given que o usuário acessa a página de produtos
+        When o usuário seleciona um produto e define a quantidade desejada
         And adiciona o produto ao carrinho
-        And o usuário verifica o carrinho de compras
         Then o sistema valida que o produto foi adicionado com sucesso
 
 
--- cypress/e2e/step_definitions/carrinhoIncluirProduto/carrinhoIncluirProduto.js
+++ cypress/e2e/step_definitions/carrinhoIncluirProduto/carrinhoIncluirProduto.js
@@ -1,16 +1,14 @@
 import CarrinhoIncluirProduto from "../../../poo/carrinhoIncluirProduto.po";
 import { Given, When, Then } from "cypress-cucumber-preprocessor/steps";
 
-Given("que o usuário visita a página de produtos", () => {
+Given("que o usuário acessa a página de produtos", () => {
   cy.autenticar();
   cy.visit(Cypress.env("BASE_URL") + "/products");
 });
 
-When("o usuário seleciona um produto", () => {
+When("o usuário seleciona um produto e define a quantidade desejada", () => {
   CarrinhoIncluirProduto.SelecionaProduto().contains("View Product").click();
-});
 
-And("insere a quantidade desejada", () => {
   CarrinhoIncluirProduto.insereQuantidadeProduto().type(
     "{selectall}{backspace}2"
   );
@@ -20,12 +18,10 @@ And("adiciona o produto ao carrinho", () => {
   CarrinhoIncluirProduto.btnAddToCart().click();
 });
 
-And("o usuário verifica o carrinho de compras", () => {
+Then("o sistema valida que o produto foi adicionado com sucesso", () => {
   CarrinhoIncluirProduto.btnVisualizaCarrinhoIframe()
     .contains("View Cart")
     .click();
-});
 
-Then("o sistema valida que o produto foi adicionado com sucesso", () => {
   CarrinhoIncluirProduto.validarProdutoAdicionado().contains("Blue Top");
 });
